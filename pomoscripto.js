d10c8089d6c86488c0483e857caff7ddf0813011
CloudTwelve <whippedreem@icloud.com>
2025-01-11 16:15:44 -0500

    added messages for being off task

---
+++ pomoscripto.js
@@ -0,0 +1,12 @@
+let messages = [
+    "That was kinda uncool.",
+    "... looks like SOMEBODY needs to finish some work.",
+    "Patience. It's a virtue.",
+    "bruh get back to work", 
+    "If you can read this, get back on task.",
+    "uh oh",
+    "what are you even going to do here?",
+    "caught in 4k... or whatever your device has lol",
+    "idk, imo you should get to work.",
+    "what's up? your work ethic should be."
+];
