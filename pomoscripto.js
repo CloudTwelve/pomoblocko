41403ef10edc3f16f02813c9d72393cc8acf7f53
CloudTwelve <whippedreem@icloud.com>
2025-01-15 23:50:26 -0500

    VERY BASIC header and font, made some donation links and made the footer donation links randomize, inshaAllah

--- pomoscripto.js
+++ pomoscripto.js
@@ -9,19 +9,26 @@ if (window.location.hostname === "www.youtube.com") {
 // user input fills inshaAllah
 let blockedSites = [];
 
-let messages = [
-    "That was kinda uncool.",
-    "... looks like SOMEBODY needs to finish some work.",
-    "Patience. It's a virtue.",
-    "bruh get back to work", 
-    "If you can read this, get back on task.",
-    "uh oh",
-    "what are you even going to do here?",
-    "caught in 4k... or whatever your device has lol",
-    "idk, imo you should get to work.",
-    "what's up? your work ethic should be."
-];
+let placesToDonateTo = [
+    ["https://irusa.org/middle-east/palestine/", "Palestine"],
+    ["https://www.launchgood.com/communitypage/syria_1#!/community/syria_1/", "Syria"],
+    ["https://sapa-usa.org/", "Sudan"],
+    ["https://www.launchgood.com/v4/campaign/empowering_rohingya_children_through_education?", "Rohingyan Children"],
+    ["https://www.launchgood.com/v4/campaign/mercy_bakery_yemen_5?src=internal_comm_page_support", "Yemen"],
+    ["https://www.launchgood.com/v4/campaign/los_angeles_wildfires_emergency_2025?src=internal_comm_page", "Los Angeles Wildfire Relief"],
+    ["https://www.launchgood.com/v4/campaign/fuel_your_health_building_a_free_clinic_in_uganda?src=", "a Ugandan Clinic"],
+    ["https://www.launchgood.com/v4/campaign/palestine_mothers_and_babies?src=internal_discover", "Palestinian Mothers and Babies"]
+]
 
 const genSiteURL = (url) => {
     return url += "/*";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    let donationLink = document.querySelector("#donation-link");
+    let num = Math.floor(Math.random() * placesToDonateTo.length);
+    donationLink.href = placesToDonateTo[num][0];
+    let messageHolder = document.querySelector(".donation");
+    let message = document.createTextNode(placesToDonateTo[num][1]);
+    messageHolder.appendChild(message);
+  });
