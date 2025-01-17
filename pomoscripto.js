7862d5de7521265e29073b0428c963376b890cda
CloudTwelve <whippedreem@icloud.com>
2025-01-16 22:56:51 -0500

    fixed JS func, inshaAllah it works

--- pomoscripto.js
+++ pomoscripto.js
@@ -57,7 +57,7 @@ if (blockedSites.includes(window.location.hostname)) {
 
     getJS().then(jsContent => {
       const scriptElement = document.createElement('script');
-      scriptElement.src = chrome.runtime.getURL('landingscript.js');
+      scriptElement.src = jsContent;
       document.body.appendChild(scriptElement);
       }).catch(error => {
         console.error("Error loading JS:", error);
