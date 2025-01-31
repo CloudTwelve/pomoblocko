5a3b65b995da617990204021b5667c253fe9d257
CloudTwelve <whippedreeeem@gmail.com>
2025-01-31 10:05:43 -0500

    just saving to work on, still has a hanging message somewhere

--- pomoscripto.js
+++ pomoscripto.js
@@ -101,15 +101,17 @@ if (!chrome.storage.local.get("breakTime")) {
             injectContent();
     }
 }
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.cmd === 'UPDATE_BREAK_STATUS') {
+    if (request.cmd === 'UPDATE_BREAK_STATUS_CONT') {
         breakTime = request.breakTime;
         if (blockedSites.includes(window.location.hostname) && !breakTime) {
             injectContent();
         } else {
             location.reload();
-        }
+        }        
         sendResponse({ status: "Break status updated" });
     }
+    sendResponse({ status: "Break status updated" });
 });
